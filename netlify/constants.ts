03ee3886226d54f7a67b20f89e4f35af19c62812
ashutosh-blockpeer <ashutosh.mehta@blockpeer.finance>
2025-11-06 16:39:28 +0530

    Update constants.ts

--- netlify/constants.ts
+++ netlify/constants.ts
@@ -8,6 +8,7 @@ export const ALLOWED_ORIGIN_REGEX =
 export const LOCALHOST_ORIGINS = [
   "http://127.0.0.1:3000",
   "http://localhost:3000",
+  "http://localhost:8000"
 ];
 
 export enum ERROR_MESSAGE {
