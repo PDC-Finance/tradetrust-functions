1bf2e12c8f5b3cd94afe692579f470ab047613f6
ashutosh-blockpeer <ashutosh.mehta@blockpeer.finance>
2025-12-24 14:58:21 +0530

    Add additional localhost origins for development

--- netlify/constants.ts
+++ netlify/constants.ts
@@ -9,6 +9,8 @@ export const ALLOWED_ORIGIN_REGEX =
 export const LOCALHOST_ORIGINS = [
   "http://127.0.0.1:3000",
   "http://localhost:3000",
+  "http://localhost:5173",
+  "http://localhost:5174",
   "http://localhost:8000",
   "http://localhost"
 ];
